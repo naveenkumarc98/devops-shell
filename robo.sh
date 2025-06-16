383842699e8f851f8d627a4d7744c064a208efa2
Naveen Kumar <r151298@gmail.com>
2025-06-16 08:26:18 +0530

    message

--- robo.sh
+++ robo.sh
@@ -9,7 +9,7 @@ DOMAIN_NAME="teluguveera.site"
 for instance in ${INSTANCES[@]}
 do
    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t2.micro --security-group-ids sg-014372f0738462686 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query "Instances[0].InstanceId" --output text)
-   if [ $instance != "frontend"]
+   if [ $instance != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
     else
