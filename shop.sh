c2155d1a74ae559b2bc2879dd6a6c19844e51518
Naveen Kumar <r151298@gmail.com>
2025-06-28 17:01:21 +0530

    Ansible

--- shop.sh
+++ shop.sh
@@ -6,8 +6,8 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 ZONE_ID="Z0010418S0UYRSNAO6BL" # replace with your ZONE ID
 DOMAIN_NAME="teluguveera.site" # replace with your domain
 
-for instance in ${INSTANCES[@]}
-#for instance in $@
+#for instance in ${INSTANCES[@]}
+for instance in $@
 do
     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-014372f0738462686 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
